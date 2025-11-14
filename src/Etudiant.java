88d6a5fc291c8326df56d2d4dca1a5dfe429f9c6
wassime el mansoury <wassimelmansoury@gmail.com>
2025-11-14 16:01:40 +0100

    Update: fixed afficherInfos formatting

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -4,16 +4,35 @@ public class Etudiant {
     private int studentId;
     private String nom;
     private String email;
+    private List<Cours> listeCours;
+
 
 
 
     public Etudiant ( int studentId , String nom , String email ){
-
         this.studentId=studentId;
         this.nom=nom;
         this.email=email;
+        this.listeCours=new ArrayList<>();
     }
     public void inscrire(Cours c) {
         c.ajouterEtudiant(this);
+        System.out.println("Étudiant" + nom + "inscrit dans le cours :" + c.gettitre());
     }
+    public int getcourseId() {
+        return courseId;
+    }
+    public String gettitre() {
+        return titre;
+    }
+    public String getdescription() {
+        return description;
+    }
+    public void afficherCours() {
+        for (Cours c : listeCours) {
+            System.out.println(c.gettitre());
+        }
+
+    }
+
 }
